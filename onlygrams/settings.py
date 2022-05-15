1c6446ffc222e7db03678dbac41d2ce937ca1f73
maroofkhan <khan.maroof6569@gmail.com>
2022-05-14 22:28:59 -0500

    working on adminpy

--- onlygrams/settings.py
+++ onlygrams/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# AUTH_USER_MODEL= 'user.User'
+AUTH_USER_MODEL= 'user.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- user/models.py
+++ user/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
 
-# Create your models here.
+class User(AbstractUser):
+    picture = models.ImageField(upload_to='profile_pictures', null=False, blank=False)
 
-# class User(AbstractUser):
-#     picture = models.ImageField(upload_to='profile_pictures')
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
 
-#     USERNAME_FIELD = 'username'
-#     REQUIRED_FIELDS = []
-
-#     objects = BaseUserManager()
+    objects = BaseUserManager()
