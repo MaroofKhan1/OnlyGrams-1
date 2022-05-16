c19db8a5c08f00e64a6a13b38387df5051df8a82
maroofkhan <khan.maroof6569@gmail.com>
2022-05-15 22:28:18 -0500

    team works make the dream work

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import URLPattern, path
-from authentication.views import SignUpView
+from authentication.views import SignUpView, home
 
 urlpatterns = [
+    path('', home, name='home_view'),
     path('signup/', SignUpView.as_view(), name='signup_view'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,25 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+# from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
+from authentication.forms import UserForm
 # Create your views here.
+
+def home(request):
+    return HttpResponse('home')
 
 class SignUpView(View):
     template_name = 'authentication/signup.html'
+    form_class = UserForm
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
     
     def post(self, request, *args, **kwargs):
-        pass
+        form = self.form_class(request.POST)
+        breakpoint()
+        if form.is_valid():
+            form.save()
+            return redirect('home_view')
+        return render(request, self.template_name)
+        
